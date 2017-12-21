7b48d5b58917ae7d89a1f213e5a18b970128d13c
Song Guo <gs199704@gmail.com>
2017-12-21 10:33:05 +0800

    translate.c 定义了一般变量和数组

--- Project2/translate.c
+++ Project2/translate.c
@@ -4,6 +4,7 @@
 #define trans( x ) void translate_T_##x
 #define translate( x , ... ) translate_T_##x( T_##x * _, ##__VA_ARGS__ )
 #define transdecl( x , ... ) void translate( x, ##__VA_ARGS__)
+#define transdecl_sizet( x , ... ) size_t translate( x, ##__VA_ARGS__)
 #define transcall( x , ... ) translate_T_##x( __VA_ARGS__ )
 #define N( x ) T_##x
 
@@ -15,8 +16,8 @@ transdecl(Specifier);
 transdecl(StructSpecifier);
 transdecl(OptTag);
 transdecl(Tag);
-transdecl(VarDec);
-transdecl(VarDimList);
+transdecl_sizet(VarDec, size_t);
+transdecl_sizet(VarDimList, size_t);
 transdecl(FunDec);
 transdecl(VarList);
 transdecl(ParamDec);
@@ -51,6 +52,13 @@ transdecl(ExtDef)
 		foreach ( _->function->varList , vlist , VarList ) son_cnt++;
 		// 计算函数参数个数
 		size_t func_type = E_trie_find(_->spec->typeName);
+		// 函数被定义了
+		if (E_trie_find(_->function->name))
+		{
+			fprintf(stderr, "Redeclare of function %s\n", _->function->name);
+			return;
+		}
+		// TODO: 处理包含结构体定义的情况
 		if (!func_type)
 		{
 			fprintf(stderr, "Unknown type %s\n", _->spec->typeName);
@@ -84,8 +92,74 @@ transdecl(ExtDef)
 		E_trie_back_to_version(current_version);
 	} else // variables
 	{
+		size_t vari_type = E_trie_find(_->spec->typeName);
+		if (!vari_type)
+		{
+			fprintf(stderr, "Unknown type %s\n", _->spec->typeName);
+			return;
+		}
+		// TODO: 处理包含结构体定义的情况
+		// 获取变量类型号
+		foreach ( _->dec, decList, ExtDecList )
+		{
+			// 单独处理每个变量
+			if (E_trie_find(decList->dec->varName))
+			{
+				// 可持久化Trie树
+				// 这是全局变量，所以直接查询当前的版本有没有定义就好了
+				fprintf(stderr, "Redeclare of %s\n", decList->dec->varName);
+				continue;
+			}
+			// 计算这个变量
+			size_t id = transcall(VarDec, decList->dec, vari_type);
+			// 加入到trie树中
+			E_trie_insert(decList->dec->varName, id);
+		}
 	}
 }
+
+transdecl_sizet(VarDec, size_t spec)
+{
+	size_t typeid = spec;
+	if (_->dim)
+	{
+		typeid = transcall(VarDimList, _->dim, spec);
+	}
+	size_t thisid = E_symbol_table_new();
+	E_symbol_table[thisid].type_uid = typeid;
+	E_symbol_table[thisid].name = _->varName;
+	E_symbol_table[thisid].len = E_symbol_table[typeid].len;
+	E_symbol_table[thisid].is_abstract = 0x002;
+	E_symbol_table[thisid].offset = 0;
+	E_symbol_table[thisid].son_cnt = 0;
+	E_symbol_table[thisid].son = NULL;
+	return thisid;
+}
+
+transdecl_sizet(VarDimList, size_t spec)
+{
+	size_t thisdim = spec;
+	size_t thisid = E_symbol_table_new();
+	char buf[512];
+	if (_->next)
+	{
+		thisdim = transcall(_->next, spec);
+		sprintf(buf, "%s/a", E_symbol_table[thisdim].name);
+	} else {
+		sprintf(buf, "%d/a", (int)spec);
+	}
+	E_symbol_table[thisid].type_uid = thisdim;
+	E_symbol_table[thisid].name = (char *)malloc(strlen(buf) + 5);
+	for (size_t i = 0; buf[i]; i++)
+		E_symbol_table[thisid].name[i] = buf[i];
+	E_symbol_table[thisid].len = E_symbol_table[typeid].len * _->thisDim;
+	E_symbol_table[thisid].is_abstract = 0x004;
+	E_symbol_table[thisid].offset = 0;
+	E_symbol_table[thisid].son_cnt = 0;
+	E_symbol_table[thisid].son = NULL;
+	return thisid;
+}
+
 
 #undef trans
 #undef transdecl
